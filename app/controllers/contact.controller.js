768d3beba4df2809803122328f26c3c1c692a369
shyns2108 <quynhanh21082k1@gmail.com>
2022-04-06 00:58:50 +0700

    Cai dat cac handler truy xuat CSDL

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,22 +1,144 @@
-exports.create = (req, res)=>{
-    res.send({message: "create handler"});
-};
-exports.findAll = (req, res)=>{
-    res.send({message: "findAll handler"});
-};
-exports.findOne = (req, res)=>{
-    res.send({message: "findOne handler"});
-};
-exports.update = (req, res)=>{
-    res.send({message: "update handler"});
-};
-exports.delete = (req, res)=>{
-    res.send({message: "delete handler"});
-};
-exports.deleteAll = (req, res)=>{
-    res.send({message: "deleteAll handler"});
+const mongoose = require("mongoose");
+const { BadRequestError } = require("../errors");
+const handlePromise = require("../helpers/promise.helper");
+const Contact = require("../models/contact.model");
+
+exports.create = async (req, res, next) => {
+
+    //Validate request
+    if(!req.body.name) {
+        return next (new BadRequestError(400, "Name can not be empty"));
+    }
+ 
+    // Create a contact
+    const contact = new Contact({
+        name: req.body.name,
+        email: req.body.email,
+        address: req.body.address,
+        phone: req.body.phone,
+        favorite:req.body.favorite === true,
+    });
+ 
+    //Save contact in the database
+    const [error, document] = await handlePromise(contact.save());
+ 
+    if (error){
+        return next(new BadRequestError(500, "An error occurred while creating the contact"));
+    }
+ 
+    return res.send(document);
+
+    //res.send({ message: "create handler"});
 };
 
-exports.findAllFavorite = (req, res)=>{
-    res.send({message: "findAllFavorite handler"});
+exports.findAll = async (req, res, next) => {
+    const condition = {  };
+    const { name } = req.query;
+    if (name){
+        condition.name = { $regex: new RegExp(name), $options: "i"};
+    }
+    const  [error, document] = await handlePromise(Contact.find(condition));
+ 
+    if(error){
+        return next(new BadRequestError(500, "An error occurred while retrieving contacts"));
+    }
+ 
+    return res.send(document);
+ 
+    //res.send({ message: "findAll handler"});
+};
+
+
+exports.findOne = async (req, res, next) => {
+    const { id } = req.params;
+    const condition = {
+        _id: id && mongoose.isValidObjectId(id) ? id: null,
+    };
+ 
+    const [ error, document] = await handlePromise(Contact.findOne(condition));
+ 
+    if(error) {
+        return next(new BadRequestError(500, `Error retrieving contact with id=${req.params.id}`));
+    }
+    if(!document){
+        return next(new BadRequestError(404, "Contact not found"));
+    }
+    return res.send(document);
+ 
+    //res.send({ message: "findOne handler"});
+};
+
+
+exports.update = async (req, res, next) => {
+ 
+    if (Object.keys(req.body).length === 0) {
+        return next(new BadRequestError(400, "Data to update can not be empty"));
+    }
+    const{ id } = req.params;
+    const condition = {
+        _id: id && mongoose.isValidObjectId(id) ? id: null,
+    };
+ 
+    const [ error, document] = await handlePromise(Contact.findOneAndUpdate(condition, req.body, {
+        new: true,
+    })
+    );
+ 
+    if(error) {
+        return next(new BadRequestError(500, `Error updating contact with id=${req.params.id}`));
+    }
+    if(!document){
+        return next(new BadRequestError(404, "Contact not found"));
+    }
+    return res.send({message: "Contact was updated successfully",});
+ 
+    //res.send({ message: "update handler"});
+};
+
+
+exports.delete = async (req, res, next) => {
+    const{ id } = req.params;
+    const condition = {
+        _id: id && mongoose.isValidObjectId(id) ? id: null,
+    };
+ 
+    const [ error, document] = await handlePromise(Contact.findOneAndDelete(condition)
+    );
+ 
+    if(error) {
+        return next(new BadRequestError(500, `Could not delete contact with id=${req.params.id}`));
+    }
+
+    if(!document){
+        return next(new BadRequestError(404, "Contact not found"));
+    }
+    
+    return res.send({message: "Contact was deleted successfully",});
+ 
+    //res.send({ message: "update handler"});
+};
+
+exports.deleteAll = async (req, res, next) => {
+    const [error, data] = await handlePromise(Contact.deleteMany({ })
+    );
+
+    if (error) {
+        return next (new BadRequestError(500, "An error occurred while removing all contacts"));
+    }
+
+    return res.send( { message: `${data.deleteCount} contacts were deleted successfully`, });
+    //res.send({ message: "findAllFavorite handler"});
+};
+
+exports.findAllFavorite = async (req, res, next) => {
+    const [error, documents] = await handlePromise(Contact.find({ 
+        favorite: true, })
+    );
+
+    if (error) {
+        return next (new BadRequestError(500, "An error occurred while retrieving favorite contacts"));
+    }
+
+    return res.send(documents);
+    //res.send({ message: "findAllFavorite handler"});
 };
